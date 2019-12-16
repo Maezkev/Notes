5ca073c629a69f77bef39b9bfe1e4691b6aacbdf
Maezkev <58494367+Maezkev@users.noreply.github.com>
2019-12-16 22:07:18 +0700

    Add files via upload

---
+++ Read_Multiple.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class triv {
+  public static void main(String[] args) {
+    Scanner inputuser = new Scanner(System.in);
+    int much = inputuser.nextInt(); // input banyaknya inputan
+    int [] inpt = new int[much];
+    for(int i = 0 ; i <= much-1 ; i++){ //sampai much-1 karena index mulai dari 0
+      inpt[i] = inputuser.nextInt();
+    }
+  }   
+}
